931b76ff253a6f64f66d44e06ba11c9c0aec9585
HanSeoungJun <67749318+HanSeongJun@users.noreply.github.com>
2021-04-25 18:42:32 +0900

    2021/04/25

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState} from 'react'
 import Dropzone from 'react-dropzone'
 import { Icon } from 'antd';
 import axios from 'axios';
 
 function FileUpload() {
-
+    const [Images, setImages] = useState([]) // 이미지를 여러개 올리기 위해 array로 
+     
     const dropHandler = (files) => {
         let formData = new FormData();
 
@@ -16,14 +17,13 @@ function FileUpload() {
         axios.post('/api/product/image',formData , config)
         .then(response => {
             if(response.data.success) {
-                
+                setImages([...Images, response.data.filePath ]) // 모든 이미지를 넣어주기 
             } else {
                 alert('파일을 저장하는데 실패하였습니다.')
             }
         })
     }
 
-     
     return (
         <div style = {{ display: 'flex', justifyContent: 'space-between' }}>
             <Dropzone onDrop={ dropHandler }>
@@ -38,7 +38,19 @@ function FileUpload() {
                         <Icon type = "plus" style = {{ fontSize: '3rem'}} />        
                     </div>
                 )}
+
             </Dropzone>
+
+            <div style = {{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
+                {Images.map( (image, index) => (
+                    <div key = {index}>
+                        <img style = {{ minWidth: '300px', width: '300px', height: '240px' }}
+                            src = {`http://localhost:5000/${image}`}
+                        />
+                    </div>
+                ))}
+            </div>
+    
         </div>
     )
 }
