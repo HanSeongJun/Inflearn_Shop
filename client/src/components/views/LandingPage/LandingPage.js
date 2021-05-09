ed1a33a509d270353c81dcdee0f2951056ed63cd
HanSeoungJun <67749318+HanSeongJun@users.noreply.github.com>
2021-05-09 23:18:54 +0900

    Check Box Filter #1

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import { Icon, Col, Card, Row, Carousel } from "antd";
 import Meta from "antd/lib/card/Meta";
 import ImageSlider from "../../utils/ImageSlider";
+import CheckBox from "./Sections/CheckBox";
+import { continents } from "./Sections/Datas";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]); // 여러가지 상품들을 넣어주기 위해 array로
@@ -71,6 +73,8 @@ function LandingPage() {
       </div>
 
       {/* Filter */}
+      {/* CheckBox */}
+      <CheckBox list={continents} />
       {/* Search */}
       {/* Cards */}
 
